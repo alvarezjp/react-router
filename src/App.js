a41e99a9e30a98bf799b6352a0917f8731e41cf7
Jesus <6.jesus.a@gmail.com>
2023-09-02 16:06:05 -0400

    barra de navegacion

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import "./assets/css/componentes/card.css";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 import Page404 from "./pages/page404";
+import Header from "./components/Header";
+import Post from "./pages/Post";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/sobre" element={<Sobre/>} />
+        <Route path="/posts/:id" element={<Post/>}></Route>
         <Route path="*" element={<Page404/>}/>
       </Routes>
     </Router>
